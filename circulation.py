8f85141b9ffe0dbefd71a7103f2960146a549666
jjkuang <sam.joey.kuang@gmail.com>
2020-03-18 16:20:09 -0400

    update. q2 still needs to be looked at

--- circulation.py
+++ circulation.py
@@ -9,16 +9,16 @@ class Circulation:
     of a Combined Cardiovascular System and a Rotary Pump, IEEE Conference on Decision and Control.
     """
 
-    def __init__(self, HR, Emax, Emin):
+    def __init__(self, HR, Emax, Emin, R1=1.0, R3=0.001):
         self.set_heart_rate(HR)
 
         self.Emin = Emin
         self.Emax = Emax
         self.non_slack_blood_volume = 250 # ml
 
-        self.R1 = 1.0 # between .5 and 2
+        self.R1 = R1 # between .5 and 2
         self.R2 = .005
-        self.R3 = .001
+        self.R3 = R3
         self.R4 = .0398
 
         self.C2 = 4.4
@@ -55,46 +55,14 @@ class Circulation:
         If the ejection phase ends in this time, the flow will remain non-zero until the next ejection phase. 
         """
 
-        xdot = []
-        # print(x[0])
-        # print(x[1])
-
         if x[1] > x[0]:
-            xdot = self.filling_phase_dynamic_matrix(t)*x
-        elif x[3] > 0 or x[0] > x[2]:
-            xdot = self.ejection_phase_dynamic_matrix(t)*x
+            a_mat = self.filling_phase_dynamic_matrix(t)
+        elif (x[3] > 0 or x[0] > x[2]):
+            a_mat = self.ejection_phase_dynamic_matrix(t)
         else:
-            xdot = self.isovolumic_phase_dynamic_matrix(t)*x
+            a_mat = self.isovolumic_phase_dynamic_matrix(t)
 
-        # el = self.elastance(t)
-        # del_dt = self.elastance_finite_difference(t)
-        # if filling_phase:
-        #
-        #     xdot_1 = del_dt*x[0]/el + (x[1]-x[0])/(self.R2*(1/el))
-        #     xdot_2 = (x[2]-x[1])/(self.R1*self.C2) - (x[1]-x[0])/(self.R2*self.C2)
-        #     xdot_3 = -(x[2]-x[1])/(self.R1*self.C3)
-        #     xdot_4 = 0
-        #
-        # elif ejection_phase:
-        #
-        #     xdot_1 = del_dt*x[0]/el - el*x[3]
-        #     xdot_2 = (x[2]-x[1])/(self.R1*self.C2)
-        #     xdot_3 = (x[3]/self.C3) - (x[2]-x[1])/(self.R1*self.C3)
-        #     xdot_4 = ((x[0]-x[2])-x[3]*(self.R3+self.R4))/self.L
-        #
-        # elif isovolumic_phase:
-        #
-        #     xdot_1 = del_dt*x[0]/el
-        #     xdot_2 = (x[2]-x[1])/(self.R1*self.C2)
-        #     xdot_3 = -xdot_1
-        #     xdot_4 = 0
-
-        # xdot.append(xdot_1)
-        # xdot.append(xdot_2)
-        # xdot.append(xdot_3)
-        # xdot.append(xdot_4)
-
-        return (xdot)
+        return np.matmul(a_mat,x)
 
 
     def isovolumic_phase_dynamic_matrix(self, t):
@@ -174,21 +142,9 @@ class Circulation:
         """
 
         ic = np.array([0,self.non_slack_blood_volume/self.C2,0,0])
-        sol = solve_ivp(self.get_derivative, [0, total_time], ic)
-        time = sol.t
-        left_ventricular_P = sol.y[0,:]
-        atrial_P = sol.y[1,:]
-        aortic_P = sol.y[2,:]
-        aortic_flow_rate = sol.y[3,:]
+        sol = solve_ivp(self.get_derivative, [0, total_time], ic, max_step=0.01, rtol=1e-5, atol=1e-8)
 
-        plt.plot(time, left_ventricular_P, 'r')
-        plt.plot(time, atrial_P, 'g')
-        plt.plot(time, aortic_P, 'b')
-        # plt.plot(time, aortic_flow_rate*self.R4, 'k')
-        # plt.legend(('Ventricular Pressure', 'Atrial Pressure', 'Arterial Pressure', 'Aortic Pressure btwn D2 and R4'))
-        plt.xlabel('Time (s)')
-        plt.ylabel('Pressure (mmHg)')
-        plt.show()
+        return [sol.t, sol.y]
 
 
     def _get_normalized_time(self, t):
@@ -199,25 +155,102 @@ class Circulation:
         return (t % self.tc) / self.Tmax
 
 
-    def calc_left_vbv(self):
+    def get_left_vbv(self, t, vpt):
         """
-        Question 4
+        Question 3
         Calculate left ventricular blood volume given results from a simulation of the
         cardiovascular system model. Assume a slack ventricular volume of 20mL.
+        Use elastance equation, E = P/(V - V0)
 
         :param t: time
-        :return: time normalized to self.Tmax (duration of ventricular contraction)
+        :param vpt: ventricular pressure at time t
+        :return: ventricular blood volume at time t
         """
-        return 0
+        el = self.elastance(t)
+        slack_vol = 20  # mL
+
+        return (vpt/el) + slack_vol
 
 
 ########################################################################################################################
-if __name__ == "__main__":
 
-    # Question 1
+def plot_q2():
+    # Instantiate Circulation instance with given const
     circ = Circulation(75, 2.0, 0.06)
 
-    # Question 2
-    circ.simulate(5)
+    # Simulate for 5 seconds
+    [time, states] = circ.simulate(5.0)
 
-    # Question 3
+    left_ventricular_P = states[0,:]
+    atrial_P = states[1,:]
+    arterial_P = states[2,:]
+    aortic_flow_rate = states[3,:]
+
+    # Aortic pressure right outside of aortic valve (btwn D2 and R4)
+    # aortic_P = arterial_P + aortic_flow_rate*circ.R4
+    aortic_P = left_ventricular_P - aortic_flow_rate*circ.R3
+
+    plt.figure()
+    plt.plot(time, left_ventricular_P)
+    plt.plot(time, atrial_P)
+    plt.plot(time, arterial_P)
+    plt.plot(time, aortic_P)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Pressure (mmHg)')
+    plt.title('Various Pressures vs. Time')
+    plt.legend(('Ventricular Pressure, x1',
+                'Atrial Pressure, x2',
+                'Arterial Pressure, x3',
+                'Aortic Pressure btwn D2 and R4'),
+               loc='upper left')
+    plt.show()
+
+
+def plot_pv_loops():
+    # Question 4
+    # Plot ventricular pressure (y) vs ventricular volume (x) to produce
+    # pressure-volume loop for a few cardiac cycles
+    # Increase R1 to 2 Ohms
+    # Use R1 = 0.5 and R3 = 0.2
+
+    # Instantiate the three Circulation models
+    circ_normal = Circulation(75, 2.0, 0.06)
+    circ_high_sys_res = Circulation(75, 2.0, 0.06, R1=2.0)
+    circ_aortic_stenosis = Circulation(75, 2.0, 0.06, R1=0.5, R3=0.2)
+
+    # Simulate for 5 seconds
+    [t_norm, states_norm] = circ_normal.simulate(5.0)
+    [t_high_res, states_high_res] = circ_high_sys_res.simulate(5.0)
+    [t_aortic_stenosis, states_aortic_stenosis] = circ_aortic_stenosis.simulate(5.0)
+
+    # Get ventricular pressures for respective Circulation models
+    vp_norm = states_norm[0,:]
+    vp_high_res = states_high_res[0,:]
+    vp_aortic_stenosis = states_aortic_stenosis[0,:]
+
+    # Get ventricular volumes for respective Circulation models
+    vv_norm = circ_normal.get_left_vbv(t_norm, vp_norm)
+    vv_high_res = circ_high_sys_res.get_left_vbv(t_high_res, vp_high_res)
+    vv_aortic_stenosis = circ_aortic_stenosis.get_left_vbv(t_aortic_stenosis, vp_aortic_stenosis)
+
+    # Plot pressure-volume loops
+    plt.figure()
+    plt.plot(vv_norm[-165:],vp_norm[-165:])
+    plt.plot(vv_high_res[-165:],vp_high_res[-165:])
+    plt.plot(vv_aortic_stenosis[-165:],vp_aortic_stenosis[-165:])
+    plt.xlabel('Ventricular Volume (mL)')
+    plt.ylabel('Ventricular Pressure (mmHg)')
+    plt.title('Pressure-Volume Loops for Various Cardiac Conditions')
+    plt.legend(('Normal, R1 = 1 mmHg s/mL',
+                'High systemic resistance, R1 = 2 mmHg s/mL',
+                'Aortic stenosis, R1 = 0.5 mmHg s/mL, R3 = 0.2 mmHg s/mL'),
+               loc='upper left')
+    plt.tight_layout()
+    plt.show()
+
+
+# Question 2
+plot_q2()
+
+# Question 4
+plot_pv_loops()
